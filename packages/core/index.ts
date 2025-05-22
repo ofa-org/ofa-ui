1006a0309fb825b38829bb36642f751bbf54e9c9
pengliang <624151713@qq.com>
2025-05-22 09:37:05 +0800

    feat:新建文档

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,6 @@
 import { makeInstaller } from '@ofa-ui/utils'
 import components from './components'
-
+import '@ofa-ui/theme/index.css'
 const installer = makeInstaller(components)
 
 export * from '@ofa-ui/components'
