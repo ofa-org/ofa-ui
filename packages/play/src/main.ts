8a8486832a494241e51db1aec3b36a13db167577
pengliang <624151713@qq.com>
2025-07-04 09:25:19 +0800

    fix: 本地测试使用组件库代码

--- packages/play/src/main.ts
+++ packages/play/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import OfaUi from 'ofa-ui'
+import OfaUi from 'ofa-ui/index.ts'
 
 createApp(App).use(OfaUi).mount('#app')
